e8bc22cad10fd4412f76827a68b05a650b807d2d
Matheus Vasconcelos <matheus.ribeiro.vasconcelos@usp.br>
2020-11-08 21:43:33 -0300

    Adicionando testes unitários da Release 1 #TWIS-17

---
+++ src/test/kotlin/br/com/twistter/controllers/RegisterControllerTest.kt
@@ -0,0 +1,56 @@
+package br.com.twistter.controllers
+
+import br.com.twistter.model.User
+import br.com.twistter.model.UserRoles
+import br.com.twistter.repository.UserRepository
+import br.com.twistter.repository.UserRolesRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert
+import org.junit.Test
+import org.springframework.security.crypto.password.PasswordEncoder
+import java.sql.Date
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import javax.servlet.http.HttpServletResponse
+
+class RegisterControllerTest {
+
+    val userRepository: UserRepository = mockk()
+    val userRolesRepository: UserRolesRepository = mockk()
+    val passwordEncoder: PasswordEncoder = mockk()
+
+    val httpServletResponse: HttpServletResponse = mockk()
+
+    val registerController: RegisterController = RegisterController(userRepository, userRolesRepository, passwordEncoder)
+
+    @Test
+    fun registerPageTest() {
+        val result = registerController.registerPage(httpServletResponse)
+
+        Assert.assertEquals(result.viewName, "signup")
+    }
+
+    @Test
+    fun doRegisterTest() {
+        val user = User(
+            1,
+            "MockUser",
+            "mockuser",
+            "mockpass",
+            "mockpass",
+            "M",
+            true,
+            Date.from(LocalDateTime.now().toInstant(ZoneOffset.of("-03:00"))),
+            setOf(UserRoles())
+        )
+
+        every { userRepository.save(user) } returns(user)
+        every { userRolesRepository.save(UserRoles()) } returns(UserRoles())
+        every { userRepository.findByLogin(user.login!!) } returns(user)
+
+        val result = registerController.doRegister(user)
+
+        Assert.assertEquals(result.viewName, "signup")
+    }
+}
